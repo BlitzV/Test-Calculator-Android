773bd7ec632e4bbf832a04b09be7420eaa71c2d3
Freddy Villegas <swatdavid1@gmail.com>
2020-10-03 13:20:03 -0400

    calculator view

--- app/src/main/java/com/test/chinchin/testcalculator/MainActivity.java
+++ app/src/main/java/com/test/chinchin/testcalculator/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
@@ -40,11 +41,30 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.spinner)
     AppCompatSpinner spinner;
 
-    @BindView(R.id.quantity)
-    EditText quantity;
-
-    @BindView(R.id.error_quantity)
-    TextInputLayout errorQuantity;
+    @BindView(R.id.button0)
+    TextView button0;
+    @BindView(R.id.button1)
+    TextView button1;
+    @BindView(R.id.button2)
+    TextView button2;
+    @BindView(R.id.button3)
+    TextView button3;
+    @BindView(R.id.button4)
+    TextView button4;
+    @BindView(R.id.button5)
+    TextView button5;
+    @BindView(R.id.button6)
+    TextView button6;
+    @BindView(R.id.button7)
+    TextView button7;
+    @BindView(R.id.button8)
+    TextView button8;
+    @BindView(R.id.button9)
+    TextView button9;
+    @BindView(R.id.buttonClear)
+    TextView buttonClear;
+    @BindView(R.id.buttonDot)
+    TextView buttonDot;
 
     @BindView(R.id.calculate)
     Button Calculate;
@@ -55,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
     private Disposable subscription;
     private ApiModel modelObject = FunctionsHelper.MockDataApiModel();
     private SpinnerAdapter spinnerAdapterData;
+    private String quantity = null;
     private int pos = 0;
     private boolean isBadResponse = false;
 
@@ -65,6 +86,31 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         App.getAppComponent().inject(this);
+
+        View.OnClickListener numberListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TextView button = (TextView) v;
+
+                if (quantity == null && !button.getText().toString().equals("0") && !button.getText().toString().equals(".")) {
+
+                    quantity = button.getText().toString();
+
+                } else if (quantity != null && quantity.length() < 11 && !quantity.matches(".*\\.[0-9]{2}?.*")) {
+
+                    quantity = quantity + button.getText().toString();
+
+                    if (quantity.indexOf(".") != (quantity.length() - 1)) {
+                        quantity = FunctionsHelper.DecimalFormat(Float.valueOf(quantity.replace(",", "")));
+                    }
+
+
+                }
+
+                CaptureQuantity();
+            }
+        };
 
         spinnerAdapterData = new SpinnerAdapter(this, getRemoteData().getData());
         spinner.setAdapter(spinnerAdapterData);
@@ -80,6 +126,44 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        button0.setOnClickListener(numberListener);
+        button1.setOnClickListener(numberListener);
+        button2.setOnClickListener(numberListener);
+        button3.setOnClickListener(numberListener);
+        button4.setOnClickListener(numberListener);
+        button5.setOnClickListener(numberListener);
+        button6.setOnClickListener(numberListener);
+        button7.setOnClickListener(numberListener);
+        button8.setOnClickListener(numberListener);
+        button9.setOnClickListener(numberListener);
+        buttonDot.setOnClickListener(numberListener);
+
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (quantity != null) {
+
+                    quantity = quantity.subSequence(0, quantity.length() - 1).toString();
+
+                    if (quantity.length() > 0) {
+
+                        if (quantity.indexOf(".") == (quantity.length() - 1)) {
+                            quantity = quantity.subSequence(0, quantity.length() - 1).toString();
+                        }
+
+                        quantity = FunctionsHelper.DecimalFormat(Float.valueOf(quantity.replace(",", "")));
+
+                    }
+
+                    CaptureQuantity();
+                }
+            }
+        });
+    }
+
+    private void CaptureQuantity() {
+        Toast.makeText(this, "si", Toast.LENGTH_SHORT).show();
     }
 
     @Override
