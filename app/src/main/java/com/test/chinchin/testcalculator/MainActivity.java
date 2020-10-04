18a5e9a56401fe34c366b01372650fe3e14f12dc
Freddy Villegas <swatdavid1@gmail.com>
2020-10-04 10:41:51 -0400

    fix

--- app/src/main/java/com/test/chinchin/testcalculator/MainActivity.java
+++ app/src/main/java/com/test/chinchin/testcalculator/MainActivity.java
@@ -230,7 +230,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        Calculate.setEnabled(false);
         SpinnerPoblate();
     }
 
