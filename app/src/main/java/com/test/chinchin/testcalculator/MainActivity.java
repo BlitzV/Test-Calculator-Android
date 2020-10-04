a75e76cc91ed0cb9c15b44df9a1544474c726516
Freddy Villegas <swatdavid1@gmail.com>
2020-10-04 11:49:38 -0400

    camera permission

--- app/src/main/java/com/test/chinchin/testcalculator/MainActivity.java
+++ app/src/main/java/com/test/chinchin/testcalculator/MainActivity.java
@@ -1,10 +1,19 @@
 package com.test.chinchin.testcalculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatSpinner;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -296,4 +305,50 @@ public class MainActivity extends AppCompatActivity {
         return modelObject;
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.read_qr:
+                if(ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA)==PackageManager.PERMISSION_GRANTED) {
+                    startActivity(new Intent(MainActivity.this,ReadQrActivity.class));
+                } else {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        requestPermissions(new String[]{Manifest.permission.CAMERA}, 22);
+                    }
+                }
+                return true;
+
+            case R.id.close_sesion:
+                return true;
+
+            default:
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 22:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startActivity(new Intent(MainActivity.this,ReadQrActivity.class));
+                } else {
+                    DialogsHelper.ShowDialogSimpleOKButton(this, getString(R.string.error_permission),
+                            getString(R.string.mensaje_permission), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).show();
+                }
+                return;
+        }
+    }
 }
---
+++ app/src/main/java/com/test/chinchin/testcalculator/ReadQrActivity.java
@@ -0,0 +1,14 @@
+package com.test.chinchin.testcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReadQrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_qr);
+    }
+}
